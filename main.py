c369c58779548837ed5d6e23c518b6f4ac53f786
VINANK <neupaneayush3@gmail.com>
2024-04-20 17:14:07 +0545

    create,read all and read by id

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app=FastAPI()
 
@@ -10,9 +11,16 @@ class Post(BaseModel):
 	published:bool=True
 	rating:int|None=None
 
-@app.get('/')
+my_post=[{"id":1,"title":"homework","content":"i did my homework"},{"id":2,"title":"title for post2","content":"content for post 2"}]
+
+def find_post(id):
+	for p in my_post:
+		if p["id"]==id:
+			return p
+
+@app.get('/posts')
 def hello():
-	return {"message":"hello world"}
+	return {"data":my_post}
 
 # @app.post('/createpost')
 # def create_post(payload:dict=Body(...)):
@@ -20,9 +28,15 @@ def hello():
 # 	return {"title":payload['title'],"content":payload['content']}
 
 
-@app.post('/createpost')
+@app.post('/posts')
 def create_post(post:Post):
-	print(post)
 	dict_post=post.dict()
-	print(dict_post)
-	return {"message":post}
+	dict_post["id"]=randrange(0,100000000)
+	my_post.append(dict_post)
+	return {"message":dict_post}
+
+
+@app.get('/posts/{id}')
+def get_post(id:int):
+	post=find_post(id)
+	return {"data":post}
