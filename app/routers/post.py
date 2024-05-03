7c5f53dd0c7d70d50ddd42bd4459ccba917f9256
VINANK <neupaneayush3@gmail.com>
2024-05-03 13:33:14 +0545

    protecting routes and adanced postaman features

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,9 +12,12 @@ router=APIRouter(
 
 
 @router.get('/',response_model=List[schemas.PostResponse])
-def get_all_posts(db: Session = Depends(get_db),user:int= Depends(oauth2.get_current_user)):
+def get_all_posts(db: Session = Depends(get_db),current_user:int= Depends(oauth2.get_current_user)):
 	# cursor.execute("""SELECT * FROM posts""")
 	# posts=cursor.fetchall()
+
+	print(current_user.email)
+
 	posts=db.query(models.Post).all()
 	return posts
 
@@ -25,7 +28,7 @@ def get_all_posts(db: Session = Depends(get_db),user:int= Depends(oauth2.get_cur
 
 
 @router.post('/',status_code=status.HTTP_201_CREATED,response_model=schemas.PostResponse)
-def create_post(post:schemas.CreatePost,db: Session = Depends(get_db)):
+def create_post(post:schemas.CreatePost,db: Session = Depends(get_db),current_user:int= Depends(oauth2.get_current_user)):
 	# cursor.execute("""INSERT INTO posts (title,content,published) VALUES(%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
 	# new_post=cursor.fetchone()
 	# conn.commit()
@@ -49,7 +52,7 @@ def get_post(id:int,response:Response,db: Session = Depends(get_db),user:int= De
 
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int,db: Session = Depends(get_db)):
+def delete_post(id:int,db: Session = Depends(get_db),current_user:int= Depends(oauth2.get_current_user)):
 	# cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id)))
 	# deleted_post=cursor.fetchone()
 	# conn.commit()
@@ -63,7 +66,7 @@ def delete_post(id:int,db: Session = Depends(get_db)):
 	return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put('/{id}',response_model=schemas.PostResponse)
-def update_post(id:int,post:schemas.CreatePost):
+def update_post(id:int,post:schemas.CreatePost,current_user:int= Depends(oauth2.get_current_user)):
 	# cursor.execute("""UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",(post.title,post.content,post.published,str(id)))
 	# updated_post=cursor.fetchone()
 	# conn.commit()
